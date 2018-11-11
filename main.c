a9a2ae0cfc8028d480c8b203483ce75d8217d7b3
Alejandro Franco <alejo4373@gmail.com>
2018-11-11 15:36:59 -0800

    reading from std + finding dimensions

--- main.c
+++ main.c
@@ -6,21 +6,57 @@
 /*   By: alfranco <alfranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 13:35:39 by alfranco          #+#    #+#             */
-/*   Updated: 2018/11/11 13:55:17 by alfranco         ###   ########.fr       */
+/*   Updated: 2018/11/11 15:36:29 by alfranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <sys/types.h>
-// #include <sys/uio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 #include <unistd.h>
+
+int		find_width(char *str)
+{
+	int count;
+	count = 0;
+	
+	while (*str != '\n')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+int		find_hight(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		if (*str == '\n')
+			count++;
+		str++;
+	}
+	return (count);
+}
 
 int		main(void)
 {
-	char ch;
-	while (read(0, &ch, 1) > 0)
+	char *str;
+	char tmp;
+	int i;
+	int bytes;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * 6400);
+	while(read(0, &tmp, 1))
 	{
-		printf("%c", ch);
+		str[i] = tmp;
+		i++;
 	}
+	str[i] = '\0';
+	printf("w: %i, h: %i", find_width(str), find_hight(str));
 }
